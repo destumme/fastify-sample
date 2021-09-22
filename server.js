ed6b32c18fa8218a28f2affd9cf48a13acd6006b
Daniel Stumme <destumme@gmail.com>
2021-09-22 07:26:01 -0500

    Minimal example showing schema object in schema definition

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const Fastify = require('fastify')
+const mercurius = require('mercurius')
+const fs = require('fs')
+const path = require('path')
+
+const app = Fastify({logger: true})
+
+const schema = fs.readFileSync(path.join(__dirname, 'schema.graphql')).toString('utf-8')
+
+const resolvers = {
+  Query: {
+    books: async (_) => [
+        {
+            title: 'Frankenstein',
+            author: 'Mary Shelley'
+        }
+    ]
+  }
+}
+
+app.register(mercurius, {
+  schema,
+  resolvers,
+  federationMetadata: true
+})
+
+app.get('/', async function (req, reply) {
+  const query = '{ books }'
+  return reply.graphql(query)
+})
+
+app.listen(3000)
